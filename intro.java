c44b87edc0970d412b386a0a26263c72b87c1f15
megath <megath@live.com>
2023-03-09 23:45:49 -0600

    ejercicios

--- intro.java
+++ intro.java
@@ -1,4 +1,3 @@
-
 package ejerciciosLogicos;
 
 import java.text.MessageFormat;
@@ -53,6 +52,14 @@ public class Practica {
 		System.out.println(msg);
 		
 		
+		// Data types
+		int myNum = 5;               // Integer (whole number)
+		float myFloatNum = 5.99f;    // Floating point number
+		char myLetter = 'D';         // Character
+		boolean myBool = true;       // Boolean
+		String myText = "Hello";     // String
+		
+		
 		int v1=55;
 		int v2=10;
 		double divi=((double)v1/(double)v2);
@@ -60,17 +67,30 @@ public class Practica {
 		
 		// Pedir 2 numeros (x), (y)
 				// Enteros
+		int n1=3, n2=7;
 				
 				// 1. Convertir a double cada numero
-				
+		
+		double n3=(double)n1;
+		double n4=(double)n2;
+		System.out.println(n3 + " - "+ n4);
 				// 2. Tomar las variables enteras y convertir a datos binarios.
-				
+		String nb1= decimalToBinary(n1);
+		String nb2= decimalToBinary(n2);
+		System.out.println(nb1+" - "+nb2);
 				// 3. Mostrar el numero mayor
-				
+		
+		System.out.println(n1>n2?n1+" es mayor":n2+" es mayor");
+		
 				// 4. Obtener el promedio de (x) y (y)
 
+		System.out.println("Promedio: "+(n1+n2)/2);
 		
-		
+	}
+
+	private static String decimalToBinary(int n1) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
