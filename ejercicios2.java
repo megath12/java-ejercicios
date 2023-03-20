092480c7909ed8e9ce6610b1329b7100c14aae43
megath <megath@live.com>
2023-03-19 23:38:51 -0600

    ejercicios

--- ejercicios2.java
+++ ejercicios2.java
@@ -1,4 +1,4 @@
-pacpackage practicaJava;
+package practicaJava;
 
 public class Practica2 {
 	
@@ -42,19 +42,47 @@ public class Practica2 {
 	    	String m=Integer.toString(n);
 	    	char e0=m.charAt(0);
 	    	char e1=m.charAt(1);
-	    	String ei=Character.toString(e1)+Character.toString(e0);
-	    	String res= m==ei ? "Es palindromo":"No es palindromo";
+	    	String res= e0==e1?"Palndromo":"No es Palidromo";
+	    	//String ei=Character.toString(e1)+Character.toString(e0);
+	    	//String res= m==ei ? "Es palindromo":"No es palindromo";
 	    	System.out.println(res);
 	    }
 	    
 	    
+	  //13. Escribir un método para remover espacios en blanco de un String. ej -> "Hola Mundo" -> "HolaMundo"
+	    public static void reme(String palabra) {
+	    	String palse = palabra.trim();
+	    	System.out.println(palse);
+	    	String revstr2 = palabra.replace(" ","");
+	    	System.out.println(revstr2);
+	    }
+	    
+	    
+	    
+	  //14. Escribir un método para invertir un String. ej -> "Hola" -> "aloH" --- Sin usar loops
+	    static void reve(String le) {
+	    	StringBuilder rev = new StringBuilder(le);
+	    	String revstr = rev.reverse().toString();
+	    	System.out.println(revstr);
+	    }
+	    
+	    
+	   
+	    
+	    
+	    
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		numPrimo(10);
 		sumBit(3,6);
 		pali(35);
 		pali(33);
+		String jh="pero que bendicion";
+		reme(jh);
+		reve(jh);
+
 	}
 
 }
+
